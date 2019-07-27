6e0a943d1fd8f037a14c1b7031e4b23edff2ecb1
Caitlyn Jules Tayco <caitlynjules_tayco@umindanao.edu.ph>
2019-07-27 16:44:20 -0700

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainLayout from './containers/MainLayout';
+import 'antd/dist/antd.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MainLayout/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import {Route, Switch} from 'react-router-dom';
+const MyRouter = () => (
+    <div>
+        <Switch>
+            <Route path="/home" component ={}/>
+        </Switch>
+    </div>
+)
+
+export default MyRouter;
